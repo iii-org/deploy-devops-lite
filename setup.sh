1d8db727b1645c9365f2f996f9f53b9ce6395f66
jonathan <tryweb@ichiayi.com>
2023-02-23 18:34:15 +0800

    依照現有 III DevOps 相關 deploy 設定調整

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ sudo sysctl -w fs.file-max=131072
 ulimit -n 131072
 ulimit -u 8192
 
-docker compose up
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install docker-ce=5:19.03.14~3-0~ubuntu-focal docker-ce-cli=5:19.03.14~3-0~ubuntu-focal docker-compose-plugin containerd.io -y
+
+docker compose up -d
