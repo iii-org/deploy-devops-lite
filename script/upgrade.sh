d60440162e3c10ad977fadc9ae66dd04b520b083
Allen <s96016641@gmail.com>
2023-03-14 18:28:08 +0800

    🐛 Fix pull policy

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -32,7 +32,6 @@ INFO "If you wish to start up your self, you are safe to exit now."
 echo -e "Press \e[96mCtrl+C\e[0m to exit, sleep 5 seconds to continue..."
 sleep 5
 
+docker compose pull
 docker compose up \
-  --pull always \
-  --quiet-pull \
   --remove-orphans -d
