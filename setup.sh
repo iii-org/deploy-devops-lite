c8a263d91f06ef3c457536ee86ed11ddcf39634c
Allen <s96016641@gmail.com>
2023-03-23 09:04:27 +0800

    ✨ Split docker install script

--- setup.sh
+++ setup.sh
@@ -54,46 +54,10 @@ command_check() {
 
   if ! command_exists docker; then
     INFO "docker could not be found, install via https://get.docker.com/"
-    echo "[+] curl -s https://get.docker.com/ | bash"
-    curl -s https://get.docker.com/ | bash
-
-    INFO "Setting up docker in rootless mode..."
-    sudo apt-get update -qq >/dev/null
-    sudo apt-get install -y -qq uidmap >/dev/null
-    dockerd-rootless-setuptool.sh install
-
-    BIN="$(dirname "$(command -v dockerd-rootless.sh)")"
-
-    # Set cap_net_bind_service capability to allow binding to privileged ports (e.g. 80, 443)
-    sudo setcap cap_net_bind_service=ep "$(which rootlesskit)"
-    systemctl --user restart docker
-
-    # Enable linger to keep the user systemd instance running
-    # From official docs https://docs.docker.com/engine/security/rootless/#daemon
-    systemctl --user start docker
-    systemctl --user enable docker
-    sudo loginctl enable-linger "$(whoami)"
-
-    export PATH=$BIN:$PATH
-    echo "export PATH=$BIN:$PATH" >>~/.bashrc
-    export DOCKER_HOST=unix://${XDG_RUNTIME_DIR}/docker.sock
-    echo "export DOCKER_HOST=unix://${XDG_RUNTIME_DIR}/docker.sock" >>~/.bashrc
-
-    # Check if dbus service is active
-    # To fix https://docs.docker.com/engine/security/rootless/#docker-run-errors
-    for i in {1..10}; do
-      if ! systemctl --user is-active --quiet dbus; then
-        WARN "dbus service is not active, trying to start it..."
-        WARN "if dbus is still inactive, please re-login to your system and run this script again"
-        systemctl --user enable --now dbus
-      else
-        INFO "dbus service is active, continue setup..."
-        break
-      fi
-    done
+    "${bin_dir:?}"/install_docker.sh
 
     # Set docker socket location, should auto detect
-    "${bin_dir:?}"/generate_env.sh docker_sock
+    "${bin_dir}"/generate_env.sh docker_sock
     source "$base_dir"/script/common.sh
 
     INFO "Install docker done! Running docker in rootless mode!"
