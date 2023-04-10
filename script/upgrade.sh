b412e1b43deb233c38935957e244d664cf960817
Allen <s96016641@gmail.com>
2023-04-10 16:08:44 +0800

    ✨ Update upgrade script

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -8,24 +8,45 @@ source "$base_dir"/common.sh
 
 cd "${project_dir:?}" || FAILED "Failed to change directory to ${project_dir:?}"
 
+update_via_git() {
+  INFO "Updating git remotes..."
+  git remote update
+
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse '@{u}')
+
+  if [ "$LOCAL" = "$REMOTE" ]; then
+    INFO "Already up-to-date"
+    exit 0
+  fi
+
+  INFO "Pulling latest changes..."
+  git pull
+}
+
+update_via_tar() {
+  INFO "Downloading latest release..."
+
+  # https://github.com/iii-org/deploy-devops-lite/archive/refs/heads/master.tar.gz
+  wget -q -O release.tar.gz https://github.com/iii-org/deploy-devops-lite/archive/refs/heads/master.tar.gz
+
+  INFO "Extracting files..."
+  tar -xzf release.tar.gz
+
+  INFO "Copying files..."
+  cp -r deploy-devops-lite-master/* .
+
+  INFO "Cleaning up..."
+  rm -rf deploy-devops-lite-master
+  rm release.tar.gz
+}
+
 # Check if .git exists
 if [ ! -d "${project_dir}"/.git ]; then
-  FAILED "This script only works for git repository"
+  update_via_tar
+else
+  update_via_git
 fi
-
-INFO "Updating git remotes"
-git remote update
-
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse '@{u}')
-
-if [ "$LOCAL" = "$REMOTE" ]; then
-  INFO "Already up-to-date"
-  exit 0
-fi
-
-INFO "Pulling latest changes"
-git pull
 
 INFO "Restart docker compose"
 INFO "If you wish to start up your self, you are safe to exit now."
