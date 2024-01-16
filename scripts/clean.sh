3c94ddd26421bb513349038709ca9eff32cf7125
Allen <s96016641@gmail.com>
2024-01-16 15:31:41 +0800

    ✨ Update clean script to support help

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,50 +5,69 @@ set -euo pipefail
 # Load common functions
 base_dir="$(cd "$(dirname "$0")" && pwd)"
 source "$base_dir"/common.sh
-
 docker_get_version
 
-cd "${PROJECT_DIR:?}" || FAILED "Failed to change directory to ${PROJECT_DIR:?}"
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [OPTION]...
 
-WARN "${ORANGE}This script will remove all data in docker volumes.${NOFORMAT}"
-WARN "${ORANGE}Please make sure you have backed up your data.${NOFORMAT}"
-echo -e "Press ${RED}Ctrl+C${NOFORMAT} to cancel, sleep 5 seconds to continue..."
-sleep 5
+Remove III DevOps Community, including all data.
 
-DOCKER_SOCKET="$(detect_docker_socket)" $DOCKER_COMPOSE_COMMAND down -v
+Options:
+  -h, --help  Print this help and exit
+EOF
+  exit 21
+}
 
-if [[ -f "${PROJECT_DIR}/.checked" ]]; then
-  rm "${PROJECT_DIR}/.checked"
-  INFO "✅ Commands check file removed"
-fi
+main() {
+  cd "${PROJECT_DIR:?}" || FAILED "Failed to change directory to ${PROJECT_DIR:?}"
 
-if [[ -d "${PROJECT_DIR}/generate" ]]; then
-  sudo rm -rf "${PROJECT_DIR}/generate"
-  INFO "✅ Generate folder removed"
-fi
+  WARN "${ORANGE}This script will remove all data in docker volumes.${NOFORMAT}"
+  WARN "${ORANGE}Please make sure you have backed up your data.${NOFORMAT}"
+  echo -e "Press ${RED}Ctrl+C${NOFORMAT} to cancel, sleep 5 seconds to continue..."
+  sleep 5
 
-if [[ -f "${PROJECT_DIR}/.env" ]]; then
-  rm "${PROJECT_DIR}/.env"
-  if [[ -f "${PROJECT_DIR}/.env.bak" ]]; then
-    rm "${PROJECT_DIR}/.env.bak"
+  DOCKER_SOCKET="$(detect_docker_socket)" $DOCKER_COMPOSE_COMMAND down -v
+
+  if [[ -f "${PROJECT_DIR}/.checked" ]]; then
+    rm "${PROJECT_DIR}/.checked"
+    INFO "✅ Commands check file removed"
   fi
-  INFO "✅ Environment config file removed"
-fi
 
-# For folders in templates, remove all .git folders
-INFO "ℹ️ Removing .git folders in templates"
-for template_dir in templates/*; do
-  if [[ -d "$template_dir" ]]; then
-    if $DOCKER_WITHOUT_SUDO; then
-      find "$template_dir" -name .git -type d -prune \
-        -exec rm -rf {} \; \
-        -exec echo -e "  -> removed \e[97m{}\e[0m" \;
-    else
-      find "$template_dir" -name .git -type d -prune \
-        -exec sudo rm -rf {} \; \
-        -exec echo -e "  -> removed \e[97m{}\e[0m" \;
+  if [[ -d "${PROJECT_DIR}/generate" ]]; then
+    sudo rm -rf "${PROJECT_DIR}/generate"
+    INFO "✅ Generate folder removed"
+  fi
+
+  if [[ -f "${PROJECT_DIR}/.env" ]]; then
+    rm "${PROJECT_DIR}/.env"
+    if [[ -f "${PROJECT_DIR}/.env.bak" ]]; then
+      rm "${PROJECT_DIR}/.env.bak"
     fi
+    INFO "✅ Environment config file removed"
   fi
-done
 
-NOTICE "Environment cleaned up, please run ${CYAN}run.sh${NOFORMAT} to re-install."
+  # For folders in templates, remove all .git folders
+  INFO "ℹ️ Removing .git folders in templates"
+  for template_dir in templates/*; do
+    if [[ -d "$template_dir" ]]; then
+      if $DOCKER_WITHOUT_SUDO; then
+        find "$template_dir" -name .git -type d -prune \
+          -exec rm -rf {} \; \
+          -exec echo -e "  -> removed \e[97m{}\e[0m" \;
+      else
+        find "$template_dir" -name .git -type d -prune \
+          -exec sudo rm -rf {} \; \
+          -exec echo -e "  -> removed \e[97m{}\e[0m" \;
+      fi
+    fi
+  done
+
+  NOTICE "Environment cleaned up, please run ${CYAN}run.sh${NOFORMAT} to re-install."
+}
+
+if [[ -n "${1-}" && "${1-}" =~ ^-h|--help$ ]]; then
+  usage
+fi
+
+main
